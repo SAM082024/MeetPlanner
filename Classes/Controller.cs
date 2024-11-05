bd9ec106803b1233933e9437fa569ce0407f1f88
SAM082024 <sam082024@gmail.com>
2024-11-05 22:39:43 +0100

    add functionality to add meets

--- Classes/Controller.cs
+++ Classes/Controller.cs
@@ -1,4 +1,10 @@
 public class Controller
 {
-    
+    private List<Model> meets = new List<Model>();
+
+    public void LeggTilMøte(string meetId, List<string> participants, DateTime timeFrame)
+    {
+        Model newMeet = new Model(meetId, participants, timeFrame);
+        meets.Add(newMeet);
+    }
 }
