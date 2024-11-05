77f56af0bdf5bbf4e697468d894b36a60ae09d93
SAM082024 <sam082024@gmail.com>
2024-11-05 22:38:11 +0100

    add properties

--- Classes/Model.cs
+++ Classes/Model.cs
@@ -1,4 +1,15 @@
 public class Model
 {
-    
+    public string? MeetId { get; set; }
+
+    public List<string> Participants { get; set; }
+
+    public DateTime TimeFrame { get; set; }
+
+    public Model(string meetId, List<string> participants, DateTime timeFrame)
+    {
+        MeetId = meetId;
+        Participants = participants;
+        TimeFrame = timeFrame;
+    }
 }
