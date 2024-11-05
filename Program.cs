591cac6445a34f44b3d5bc32fa5d66f7aa569911
SAM082024 <sam082024@gmail.com>
2024-11-05 22:33:22 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace MeetPlanner;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller();
+
+        
+    }
+}
