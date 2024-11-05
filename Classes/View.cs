975ffee8748798a11eba5f2b454ae4296c2c7710
SAM082024 <sam082024@gmail.com>
2024-11-05 22:46:42 +0100

    add dependancies and userinput

--- Classes/View.cs
+++ Classes/View.cs
@@ -1,4 +1,20 @@
 public class View
 {
-    
+    private Controller controller = new Controller();
+
+    public void AskForMeetInfo()
+    {
+        Console.WriteLine("Skriv inn møte-ID:");
+        string meetID = Console.ReadLine();
+
+        Console.WriteLine("Skriv inn navn på deltagerne (kommaseparert):");
+        List<string> participants = new List<string>(Console.ReadLine().Split(','));
+
+        Console.WriteLine("Skriv inn tidspunkt for møtet (YYYY-MM-DD HH:MM):");
+        DateTime timeFrame = DateTime.Parse(Console.ReadLine());
+
+        controller.LeggTilMøte(meetID, participants, timeFrame);
+    }
+
+
 }
